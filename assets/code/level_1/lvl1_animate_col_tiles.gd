4f7ecabc07b95962f67296c7cd3cfad24eb50b25
andzedwick <54032094+andzedwick@users.noreply.github.com>
2020-11-05 22:21:47 -0600

    Code and scene editions

--- assets/code/level_1/lvl1_animate_col_tiles.gd
+++ assets/code/level_1/lvl1_animate_col_tiles.gd
@@ -1,7 +1,17 @@
+#-----------------------------------------------------------------------------#
+# File Name:   	lvl1_animate_col_tiles.gd                                     #
+# Description: 	Animates tiles for the rooftop level collidable tiles         #
+# Author:      	Andrew Zedwick                                                #
+# Company:    	Sidetrack                                                     #
+# Last Updated:	November 5th, 2020                                            #
+#-----------------------------------------------------------------------------#
 extends Animate_Tiles
 
+#-----------------------------------------------------------------------------#
+#                             Public Functions                                #
+#-----------------------------------------------------------------------------#
+# Overwrite the animate_tiles() function to animate specific collidable tiles
 func animate_tiles():
 	animate_tile(13, 7,  1, 1) # Animate fan tiles - code 13
-	animate_tile(15, 14, 1, 3) # Animate concrete_a tiles - code 15
-	animate_tile(16, 14, 1, 3) # Animate concrete_b tiles - code 16
+	animate_tile(15, 13, 1, 3) # Animate concrete_c tiles - code 15
 	animate_tile(17, 8,  2, 5) # Animate medium fan tiles - code 17
