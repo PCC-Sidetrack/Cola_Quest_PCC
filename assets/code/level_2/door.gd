ec4c13f5fcedb9392e6e59164ed28f6addce14f2
Eric Cherubin <32425594+ericjcherubin@users.noreply.github.com>
2020-12-09 12:24:04 -0500

    Update door.gd

--- assets/code/level_2/door.gd
+++ assets/code/level_2/door.gd
@@ -9,14 +9,17 @@
 extends Area2D
 
 #-----------------------------------------------------------------------------#
-#                                Variables                                    #
+#                           Exported Variables                                #
 #-----------------------------------------------------------------------------#
+# The scene that will be switched to
 export (String, FILE) var target_scene
 
 
 #-----------------------------------------------------------------------------#
-#                            Physics/Process                                  #
+#                           Private Functions                                 #
 #-----------------------------------------------------------------------------#
+# Compares to see if the overlapping body is other than parallaxing and error
+# checks if there is a scene to switch to
 func _input(event):
 	if event.is_action_pressed("ui_up"):
 		if (get_overlapping_bodies().size()) > 0:
