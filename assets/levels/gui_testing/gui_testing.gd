9a8398660c00e11100983041694b3f4315e00985
Overduet <Aj.cizJac.Pzx21.2cdl3@gmail.com>
2021-02-05 19:26:06 -0600

    added boss ui, added boss functions, added cola counter at level cleared, added flash effect, gui bug fix

---
+++ assets/levels/gui_testing/gui_testing.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("player/game_UI").on_initialize_boss(10, "cookie")
