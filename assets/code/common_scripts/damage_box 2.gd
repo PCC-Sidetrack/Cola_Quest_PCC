712ac46f8978c44978e59ed698f2cf71f084a223
jeffnewell <jnewel4304@students.pcci.edu>
2021-03-24 10:15:19 -0500

    Character attacking update
    Updated the sprite for attacking to match the hitbox

---
+++ assets/code/common_scripts/damage_box 2.gd
@@ -0,0 +1,53 @@
+#-----------------------------------------------------------------------------#
+# Class Name:   damage_box
+# Description:  Deals damage to the player if entered, then teleports the player
+#               to their last spawn point.
+# Author:       Andrew Zedwick
+# Company:      Sidetrack
+# Last Updated: 2/22/2021
+#-----------------------------------------------------------------------------#
+
+extends Node
+
+#-----------------------------------------------------------------------------#
+#                              Exported Variables                             #
+#-----------------------------------------------------------------------------#
+# Determines how much damage the damage_box deals
+export var damage:           int   = 1
+# Determines how much knockback is applied to the player
+export var knockback:        float = 10.0
+# Determines if the damage_box deals damage
+export var deals_damage:     bool  = true
+# Determines if the damage_box causes knockback
+export var causes_knockback: bool  = true
+# Determines if the player is sent to the previous spawn point
+export var tp_to_spawn:      bool  = true
+
+# _ready method (called when the node and child nodes this script is connected to
+# are initialized and ready to be used)
+func _ready() -> void:
+	$AudioStreamPlayer2D.play()
+
+# Returns the knockback multiplier of the sword
+# Used in the knockback function called in _on_sword_body_entered to tell the player how
+# far to be knocked back.
+func get_knockback_multiplier() -> float:
+	return knockback
+
+# When the collision box is entered, check if the player entered it before dealing
+# damage.
+func _on_damage_box_body_entered(body):
+	if body.is_in_group(Globals.GROUP.PLAYER) && body is Entity:
+		if deals_damage:
+			body.take_damage(damage)
+		
+		if causes_knockback: body.knockback  (self, Vector2.UP)
+		if tp_to_spawn and Globals.player.get_current_health() != 0:
+			# Wait to respawn for a moment
+			Globals.game_locked = true
+			yield(get_tree().create_timer(0.3), "timeout")
+			body.global_position = body.get_spawn_point()
+			yield(get_tree().create_timer(0.3), "timeout")
+			Globals.game_locked = false
+		
+		
---
+++ assets/gui/hud/hud 2.gd
@@ -0,0 +1,106 @@
+#-----------------------------------------------------------------------------#
+# Class Name:   hud.gd
+# Description:  Controls elements on the in-game UI
+# Author:       Rightin Yamada                
+# Company:      Sidetrack
+# Last Updated: March 18, 2021
+#-----------------------------------------------------------------------------#
+
+extends Control
+
+#-----------------------------------------------------------------------------#
+#                                 Signals                                     #
+#-----------------------------------------------------------------------------#
+# COMMENT NEEDED
+signal cola_healing()
+
+#-----------------------------------------------------------------------------#
+#                                Variables                                    #
+#-----------------------------------------------------------------------------#
+# Counts the amount of respawns
+var _respawn_count:    int = 0
+
+# Counts the amount of cola collected 
+var _cola_count:       int = 0
+
+# COMMENT NEEDED
+var _cola_requirement: int = 5
+
+# COMMENT NEEDED
+var _m_health:         int = 0
+
+# COMMENT NEEDED
+var _c_health:         int = 0
+
+#-----------------------------------------------------------------------------#
+#                              Initialization                                 #
+#-----------------------------------------------------------------------------#
+# Hide respawn counter by default
+func _ready() -> void:
+	$ui_stat/stats/respawn_counter.visible         = false
+	$ui_stat/stats/total_cola_collected.visible    = false
+	$ui_element/cola_healing/green_plus.max_value  = _cola_requirement
+	$ui_element/cola_healing/green_plus.value      = 0
+	$ui_element/cola_healing/green_plus_ss.visible = false
+
+#-----------------------------------------------------------------------------#
+#                            Physics/Process Loop                             #
+#-----------------------------------------------------------------------------#
+# Keep ui statistics updated
+func _physics_process(_delta: float) -> void:
+	$ui_element/cola_counter/cola_collected.set_text (" " + str(_cola_count))
+	$ui_stat/stats/fps_counter.set_text              ("FPS: " + str(Engine.get_frames_per_second()))
+	$ui_stat/stats/total_cola_collected.set_text     ("Cola Collected : " + str(_cola_count))
+	$ui_stat/stats/respawn_counter.set_text          ("Respawn Counter: " + str(_respawn_count))
+
+#-----------------------------------------------------------------------------#
+#                             Trigger Functions                               #
+#-----------------------------------------------------------------------------#
+# On player killed, show respawn counter
+func _on_game_UI_player_killed() -> void:
+	yield(get_tree().create_timer(1.0), "timeout")
+	$ui_stat/stats/respawn_counter.visible = true
+
+# On player respawn, increment respawn counter
+func _on_game_UI_respawn_player() -> void:
+	$ui_element/cola_healing/green_plus.value    = 0
+	_respawn_count += 1
+	$ui_stat/stats/respawn_counter.visible       = false
+	$ui_stat/stats/total_cola_collected.visible  = false
+
+# COMMENT NEEDED
+func _on_game_UI_level_cleared():
+	yield(get_tree().create_timer(3.0), "timeout")
+	$ui_stat/stats/total_cola_collected.visible  = true
+	$ui_stat/stats/respawn_counter.visible       = true
+
+# COMMENT NEEDED
+func _on_game_UI_cola_collect(amount):
+	_cola_count                               += amount
+	$ui_element/cola_healing/green_plus.value += amount
+	$ui_element/cola_counter/cola_tween.interpolate_property($ui_element/cola_counter/cola_icon, "scale", Vector2(2,2), Vector2(1,1), .5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	$ui_element/cola_counter/cola_tween.start()
+	if ($ui_element/cola_healing/green_plus.value == $ui_element/cola_healing/green_plus.max_value):
+		$ui_element/cola_healing/green_plus_ss.visible = true
+		$ui_element/cola_healing/green_plus.visible    = false
+	
+	if ($ui_element/cola_healing/green_plus.value == _cola_requirement) and (_c_health != _m_health):
+		_cola_healing()
+
+# COMMENT NEEDED
+func _on_game_UI_initialize_player(max_health) -> void:
+	_m_health = max_health
+	_c_health = max_health
+
+# COMMENT NEEDED
+func _on_game_UI_player_health_changed(_current_health, _previous_health) -> void:
+	_c_health = _current_health
+	if ($ui_element/cola_healing/green_plus.value == _cola_requirement) and (_current_health < _previous_health):
+		_cola_healing()
+
+# COMMENT NEEDED
+func _cola_healing():
+	emit_signal("cola_healing")
+	$ui_element/cola_healing/green_plus.value      = 0
+	$ui_element/cola_healing/green_plus_ss.visible = false
+	$ui_element/cola_healing/green_plus.visible    = true
